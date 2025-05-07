2e32065b81a4dda9b6fdff55005f3044ad12bd43
chrissnts <christiandossantos125@gmail.com>
2025-05-07 09:49:20 -0300

    MEUDEUS COMO EU AMO SER BACKEND, EU NASCI PRA ISSO!!

--- dw/atvd_007/assets/js/script.js
+++ dw/atvd_007/assets/js/script.js
@@ -1,14 +1,17 @@
-let valS = 00;
-let valM = 00;
-let valH = 00;
+let valS = 0;
+let valM = 0;
+let valH = 0;
 let id;
+let posicao = 0;
+let contando = false;
+let pausando = false;
+
 
 function add() {
 
     if (valS >= 60) {
         valS = 0;
         valM++;
-
     }
 
     if (valM >= 60) {
@@ -36,10 +39,44 @@ function add() {
     } else {
         document.querySelector("#valH").textContent = valH;
     }
-
-
 }
 
 function contar() {
     id = setInterval(add, 1000)
+    contando = true;
+    pausando = false;
 }
+
+function pararContar() {
+    clearInterval(id);
+    contando = false;
+    pausando = true;
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+        if (contando) {
+            pararContar();
+        } else {
+            contar();
+        }
+    }
+});
+
+let nave = document.getElementById("naveAmiga");
+
+document.addEventListener('keydown', function (event) {
+
+    if (pausando == false) {
+        if (event.key == 'ArrowRight' && posicao < 700) {
+            posicao += 20;
+            nave.style.left = posicao + 'px';
+        }
+
+        if (event.key == 'ArrowLeft' && posicao > -800) {
+            posicao -= 20;
+            nave.style.left = posicao + 'px';
+        }
+    }
+
+});
