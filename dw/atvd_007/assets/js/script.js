0b352f022d5ca65ed738bca6512c76cdaa360e05
chrissnts <christiandossantos125@gmail.com>
2025-05-10 18:56:46 -0300

    atvd_007 update that big did

--- dw/atvd_007/assets/js/script.js
+++ dw/atvd_007/assets/js/script.js
@@ -5,6 +5,9 @@ let id;
 let posicao = 0;
 let contando = false;
 let pausando = false;
+const velocidadeTiro = 10;
+const intervalo = 30;
+const shots = [];
 
 
 function add() {
@@ -60,8 +63,10 @@ document.addEventListener("keydown", function (event) {
     if (event.key === "Escape") {
         if (contando) {
             pararContar();
+            document.getElementById("paused").style.display = 'flex';
         } else {
             contar();
+            document.getElementById("paused").style.display = 'none';
         }
     }
 });
@@ -82,3 +87,64 @@ document.addEventListener('keydown', function (event) {
         }
     }
 });
+
+document.addEventListener('keydown', function (event) {
+    if (event.key == ' ') {
+        shot();
+    }
+});
+
+
+function shot() {
+    const shot = document.createElement('div');
+    shot.classList.add('shot');
+
+
+    const contentRect = document.querySelector('#content').getBoundingClientRect();
+    const naveRect = nave.getBoundingClientRect();
+
+    console.log(contentRect.left);
+    console.log(naveRect.left);
+    console.log(naveRect.left - contentRect.left + naveRect.width / 2 - 5);
+    shot.style.left = `${(naveRect.left - contentRect.left + naveRect.width / 2 - 5) + 293}px`;
+    shot.style.top = `${naveRect.top - contentRect.top - 20}px`;
+
+
+    shot.style.position = 'absolute';
+    document.querySelector('#content').appendChild(shot);
+    shots.push({
+        element: shot,
+        direcao: 'subindo'
+
+    });
+
+}
+
+
+function moveShots() {
+    for (let i = shots.length - 1; i >= 0; i--) {
+        const shot = shots[i];
+        const el = shot.element;
+        let top = parseInt(el.style.top);
+
+        if (shot.direcao === 'subindo') {
+            top -= velocidadeTiro;
+            if (top <= 0) {
+
+                shot.direcao = 'descendo';
+            }
+        } else {
+            top += velocidadeTiro;
+            if (top > window.innerHeight - 35) {
+                el.remove();
+                shots.splice(i, 1);
+                continue;
+            }
+        }
+
+        el.style.top = `${top}px`;
+    }
+}
+
+
+setInterval(moveShots, intervalo);
