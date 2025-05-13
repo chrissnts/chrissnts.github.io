f4195e01bd657f50abe0ff93aad2e8ab2ea59a9a
chriss fwaeh <160748564+chrissnts@users.noreply.github.com>
2025-05-13 16:49:38 -0300

    update

--- dw/atvd_007/assets/js/script.js
+++ dw/atvd_007/assets/js/script.js
@@ -8,7 +8,15 @@ let pausando = false;
 const velocidadeTiro = 10;
 const intervalo = 30;
 const shots = [];
+let life = 3;
+let score = 0;
+let velocidadeQueda = 5;
+let intervaloInimigos; 
 
+window.onload = function () {
+    contar();
+    moverNaveInimiga();
+};
 
 function add() {
 
@@ -64,9 +72,11 @@ document.addEventListener("keydown", function (event) {
         if (contando) {
             pararContar();
             document.getElementById("paused").style.display = 'flex';
+           
         } else {
             contar();
             document.getElementById("paused").style.display = 'none';
+            
         }
     }
 });
@@ -90,12 +100,14 @@ document.addEventListener('keydown', function (event) {
 
 document.addEventListener('keydown', function (event) {
     if (event.key == ' ') {
+        
         shot();
     }
 });
 
 
 function shot() {
+    if (pausando) return;
     const shot = document.createElement('div');
     shot.classList.add('shot');
 
@@ -120,8 +132,11 @@ function shot() {
 
 }
 
-
 function moveShots() {
+    if (pausando) return;
+
+    const inimigas = document.querySelectorAll("#naveInimiga");
+
     for (let i = shots.length - 1; i >= 0; i--) {
         const shot = shots[i];
         const el = shot.element;
@@ -129,22 +144,123 @@ function moveShots() {
 
         if (shot.direcao === 'subindo') {
             top -= velocidadeTiro;
-            if (top <= 0) {
+            el.style.top = `${top}px`;
 
-                shot.direcao = 'descendo';
-            }
-        } else {
-            top += velocidadeTiro;
-            if (top > window.innerHeight - 35) {
+            if (top < -20) {
                 el.remove();
                 shots.splice(i, 1);
                 continue;
             }
-        }
 
-        el.style.top = `${top}px`;
+            const shotRect = el.getBoundingClientRect();
+
+
+            for (let j = 0; j < inimigas.length; j++) {
+                const inimiga = inimigas[j];
+
+                if (inimiga.style.display === "none") continue;
+
+                const inimigaRect = inimiga.getBoundingClientRect();
+
+                if (
+                    shotRect.bottom >= inimigaRect.top &&
+                    shotRect.top <= inimigaRect.bottom &&
+                    shotRect.left <= inimigaRect.right &&
+                    shotRect.right >= inimigaRect.left
+                ) {
+                    
+                    el.remove();
+                    shots.splice(i, 1);
+                    
+                    inimiga.style.visibility = "hidden";
+            
+                    score += 1;
+                    document.getElementById("score").textContent = "ALIEN: " + score;
+                    changeScreen(score);
+    
+                    break;
+                }
+            }
+        }
     }
 }
 
 
 setInterval(moveShots, intervalo);
+
+
+function moverNaveInimiga() {
+    const inimiga = document.getElementById("containerEnemies");
+    const amiga = document.getElementById("naveAmiga");
+
+    intervaloInimigos = setInterval(() => {
+        if (pausando) return; 
+
+        const inimigaTop = parseInt(getComputedStyle(inimiga).top || 0);
+        inimiga.style.top = (inimigaTop + velocidadeQueda) + "px";
+
+        const inimigaRect = inimiga.getBoundingClientRect();
+        const amigaRect = amiga.getBoundingClientRect();
+
+        if (inimigaRect.bottom >= amigaRect.top &&
+            inimigaRect.left < amigaRect.right &&
+            inimigaRect.right > amigaRect.left) {
+
+            clearInterval(intervaloInimigos);
+            life--;
+            if (life == 0) {
+                document.getElementById("paused").style.display = "flex";
+                document.getElementById("paused").innerHTML = "<p>GAME OVER</p>";
+                setTimeout(() => location.reload(), 3000);
+            } else {
+                document.querySelector("#life").textContent = "LIFE: " + life;
+                inimiga.style.top = 0;
+                moverNaveInimiga();
+            }
+        }
+    }, 30);
+}
+
+
+
+function changeScreen(score) {
+    const body = document.body;
+
+    if (score === 3) {
+        body.className = "level2";
+        velocidadeQueda += 2;
+        life = 3;
+        document.querySelector("#life").textContent = "LIFE: " + life;
+        reiniciarInimigos();
+    } else if (score === 6) {
+        body.className = "level3";
+        velocidadeQueda += 2;
+        life = 3;
+        document.querySelector("#life").textContent = "LIFE: " + life;
+        reiniciarInimigos();
+    } else if (score === 9) {
+        body.className = "level4";
+        velocidadeQueda += 2;
+        life = 3;
+        document.querySelector("#life").textContent = "LIFE: " + life;
+        reiniciarInimigos();
+    }else if (score === 12){
+        clearInterval(intervaloInimigos);
+        document.getElementById("paused").style.display = "flex";
+        document.getElementById("paused").innerHTML = "<p>YOU WIN</p>";
+    }
+}
+
+function reiniciarInimigos() {
+    const inimigas = document.querySelectorAll("#naveInimiga");
+    const container = document.getElementById("containerEnemies");
+
+    clearInterval(intervaloInimigos);
+
+    container.style.top = "0"; 
+    inimigas.forEach(inimiga => {
+        inimiga.style.visibility = "visible";
+    });
+
+    moverNaveInimiga();
+}
