b1b882bac62401df2c2345839456d8857109c62e
chrissnts <christiandossantos125@gmail.com>
2025-06-03 20:58:40 -0300

    update - installing svelte

---
+++ dw/atvd_010/App.svelte
@@ -0,0 +1,21 @@
+<script>
+    let value1 = 10
+    let value2 = 2
+
+
+    function multiplicacao(){
+        if(value2){
+            value1 = value1 * 2
+        }else{
+            
+        }
+    }
+
+    function adicao(){
+        if(value2){
+            value1 = value1 + value2
+        }
+    }
+
+
+</script>
