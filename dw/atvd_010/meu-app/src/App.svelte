4dd62d041a8955ae5a575fbdd5a0ad75a6885bfb
chrissnts <christiandossantos125@gmail.com>
2025-06-03 21:01:57 -0300

    update

---
+++ dw/atvd_010/meu-app/src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+    import Display from "./Display.svelte";
+
+	let value1 = 10;
+	let value2 = 2;
+
+	function multiplicacao() {
+		if (value2) {
+			value1 = value1 * 2;
+		} else {
+		}
+	}
+
+	function adicao() {
+		if (value2) {
+			value1 = value1 + value2;
+		}
+	}
+
+	export Display;
+</script>
+
+<main>
+	
+	<h1>Hello {name}!</h1>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
