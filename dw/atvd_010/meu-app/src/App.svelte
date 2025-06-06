e4db2877f6fbc7dcf2f1f270d88d2937eb08849b
chrissnts <christiandossantos125@gmail.com>
2025-06-06 10:04:29 -0300

    atvd_010 almost finished

--- dw/atvd_010/meu-app/src/App.svelte
+++ dw/atvd_010/meu-app/src/App.svelte
@@ -1,52 +1,101 @@
 <script>
-    import Display from "./Display.svelte";
+  let display = "";
 
-	let value1 = 10;
-	let value2 = 2;
+  function appendNumber(num) {
+    display += num;
+  }
 
-	function multiplicacao() {
-		if (value2) {
-			value1 = value1 * 2;
-		} else {
-		}
-	}
+  function appendSymbol(symbol) {
+    display += symbol;
+  }
 
-	function adicao() {
-		if (value2) {
-			value1 = value1 + value2;
-		}
-	}
+  function clear() {
+    display = "";
+  }
 
-	export Display;
+  function calculate() {
+    display = eval(display).toString();
+  }
 </script>
 
 <main>
+  <div class="container">
 	
-	<h1>Hello {name}!</h1>
-	<p>
-		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-		how to build Svelte apps.
-	</p>
+    <div class="display">
+      <p>{display || 0}</p>
+    </div>
+
+    <div class="keyboard">
+      <button on:click={() => appendNumber(1)}>1</button>
+      <button on:click={() => appendNumber(2)}>2</button>
+      <button on:click={() => appendNumber(3)}>3</button>
+      <button on:click={() => appendSymbol("+")}>+</button>
+
+      <button on:click={() => appendNumber(4)}>4</button>
+      <button on:click={() => appendNumber(5)}>5</button>
+      <button on:click={() => appendNumber(6)}>6</button>
+      <button on:click={() => appendSymbol("-")}>-</button>
+
+      <button on:click={() => appendNumber(7)}>7</button>
+      <button on:click={() => appendNumber(8)}>8</button>
+      <button on:click={() => appendNumber(9)}>9</button>
+      <button on:click={() => appendSymbol("*")}>*</button>
+
+      <button on:click={calculate}>=</button>
+      <button on:click={() => appendNumber(0)}>0</button>
+      <button on:click={clear}>C</button>
+      <button on:click={() => appendSymbol("/")}>/</button>
+    </div>
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+	margin-top: 100px;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .container {
+    width: 100%;
+    max-width: 300px;
+  }
+
+  .display {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 2px solid green;
+    background-color: white;
+    display: grid;
+    grid-template-columns: 1fr;
+    min-height: 70px;
+    border-radius: 5px;
+	margin-bottom: 20px;
+  }
+
+  .display p {
+    color: goldenrod;
+    font-size: 2em;
+	text-align: left;
+    margin-left: 20px;
+  }
+
+  .keyboard {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 10px;
+  }
+
+  button {
+    padding: 1em;
+	font-size: 1.5em;
+    background-color: green;
+    color: white;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+
 </style>
